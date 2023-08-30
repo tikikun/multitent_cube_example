33f33d0e88cceb5e0ff880209bfa0672fdd04b3b
Alan Dao <contact@daogiatuan.com>
2023-08-30 21:55:41 +0700

    change for the demo of postgre multi tenancy

---
+++ cube_conf/cube.js
@@ -0,0 +1,37 @@
+module.exports = {
+  contextToAppId: ({ securityContext }) => {
+    if (securityContext) {
+      return `CUBEJS_APP_${securityContext.appId}_${securityContext.userId}`;
+    } else {
+      return `CUBEJS_APP_1_1`;
+    }
+  },
+  contextToOrchestratorId: ({ securityContext }) => {
+    if (securityContext) {
+      return `CUBEJS_APP_${securityContext.appId}_${securityContext.userId}`;
+    } else {
+      return `CUBEJS_APP_1_1`;
+    }
+  },
+  driverFactory: ({ securityContext }) => {
+    if (securityContext === null) {
+      // handle the case without context
+      return {
+        type: "postgres",
+        database: "postgres",
+        host: `tenant_1_1`,
+        user: "postgres",
+        password: "password123",
+      };
+    } else {
+      // handle the jaw authen case
+      return {
+        type: "postgres",
+        database: "postgres",
+        host: `tenant_${securityContext.appId}_${securityContext.userId}`,
+        user: "postgres",
+        password: "password123",
+      };
+    }
+  },
+};
